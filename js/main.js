643dfb73e3e6c1526dfb8df3ad3adae545975780
Alex Tiutiunyk <alex.tiutiunyk@gmail.com>
2024-11-05 20:49:10 +0200

    add welcome text

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-const btn_1 = document.querySelector('.btn-1');
-const btn_2 = document.querySelector('.btn-2');
-const btn_3 = document.querySelector('.btn-3');
-const btn_4 = document.querySelector('.btn-4');
+const btn_1 = document.querySelector('#step-1 .btn');
+const btn_2 = document.querySelector('#step-2 .btn');
+const btn_3 = document.querySelector('#step-3 .btn');
+const btn_4 = document.querySelector('#step-4 .btn');
 
 const step_1 = document.querySelector('#step-1');
 const step_2 = document.querySelector('#step-2');
@@ -17,21 +17,23 @@ btn_2.addEventListener('click', step3Visible);
 btn_3.addEventListener('click', step4Visible);
 
 function step2Visible() {
-  const name = inputName.value.trim();
-  if (name) {
-    outName.innerHTML = name;
-    step_1.classList.remove('active');
-    step_1.classList.add('done');
-    step_2.classList.add('active');
-    btn_1.removeEventListener('click', step2Visible);
-  }
+  step_1.classList.remove('active');
+  step_1.classList.add('done');
+  step_2.classList.add('active');
+  btn_1.removeEventListener('click', step2Visible);
 }
 
 function step3Visible() {
-  step_2.classList.remove('active');
-  step_2.classList.add('done');
-  step_3.classList.add('active')
-  btn_2.removeEventListener('click', step2Visible);
+  const name = inputName.value.trim();
+  const nameRegex = /^[А-Яа-яЇїІіЄєҐґA-Za-z]+$/;
+
+  if (name && nameRegex.test(name)) {
+    outName.innerHTML = name;
+    step_2.classList.remove('active');
+    step_2.classList.add('done');
+    step_3.classList.add('active')
+    btn_2.removeEventListener('click', step2Visible);
+  }
 }
 
 function step4Visible() {

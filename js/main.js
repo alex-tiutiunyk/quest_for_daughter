693bb086217593991935ebd07d571c346b83cdc6
Alex Tiutiunyk <alex.tiutiunyk@gmail.com>
2024-11-06 23:47:24 +0200

    added step with dragon

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ const btn_4 = document.querySelector('#step-4 .btn');
 const btn_5 = document.querySelector('#step-5 .btn');
 const btn_6 = document.querySelector('#step-6 .btn');
 const btn_7 = document.querySelector('#step-7 .btn');
+const btn_8 = document.querySelector('#step-8 .btn');
+const btn_9 = document.querySelector('#step-9 .btn');
 
 const step_1 = document.querySelector('#step-1');
 const step_2 = document.querySelector('#step-2');
@@ -13,10 +15,12 @@ const step_4 = document.querySelector('#step-4');
 const step_5 = document.querySelector('#step-5');
 const step_6 = document.querySelector('#step-6');
 const step_7 = document.querySelector('#step-7');
+const step_8 = document.querySelector('#step-8');
+const step_9 = document.querySelector('#step-9');
 
 const inputName = document.getElementById('inputName');
 const inputPass1 = document.getElementById('inputPass1');
-inputPass1
+const inputPass2 = document.getElementById('inputPass2');
 
 const outName = document.getElementById('outName');
 
@@ -26,6 +30,8 @@ btn_3.addEventListener('click', step4Visible);
 btn_4.addEventListener('click', step5Visible);
 btn_5.addEventListener('click', step6Visible);
 btn_6.addEventListener('click', step7Visible);
+btn_7.addEventListener('click', step8Visible);
+btn_8.addEventListener('click', step9Visible);
 
 function step2Visible() {
   step_1.classList.remove('active');
@@ -63,7 +69,7 @@ function step6Visible() {
   // if (password === '61930') {
     step_5.classList.remove('active');
     step_5.classList.add('done');
-    step_6.classList.add('active')
+    step_6.classList.add('active');
   // }
 }
 
@@ -71,4 +77,20 @@ function step7Visible() {
   step_6.classList.remove('active');
   step_6.classList.add('done');
   step_7.classList.add('active')
+}
+
+function step8Visible() {
+  step_7.classList.remove('active');
+  step_7.classList.add('done');
+  step_8.classList.add('active')
+}
+
+function step9Visible() {
+  const password = inputPass2.value.trim();
+
+  if (password === '0816' || password === '1608') {
+    step_8.classList.remove('active');
+    step_8.classList.add('done');
+    step_9.classList.add('active')
+  }
 }
